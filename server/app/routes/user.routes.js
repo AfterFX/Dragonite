b81f4f287b4dfbfd4dd324263443b536f37cfc39
AfterFX <afterfx1995@gmail.com>
2020-10-29 22:21:59 +0200

    deleted tutorial router and moved to user router

--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -1,5 +1,7 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
+const { tutorials } = require("../controllers");
+
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
@@ -29,4 +31,50 @@ module.exports = function(app) {
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.adminBoard
   );
+
+
+
+
+  // Create a new Tutorial
+  app.post("/api/tutorials/",
+    [authJwt.verifyToken],
+    tutorials.create
+  );
+
+  // Retrieve all Tutorials
+  app.get("/api/tutorials/",
+    [authJwt.verifyToken],
+    tutorials.findAll
+  );
+
+  // Retrieve all published Tutorials
+  app.get("/api/tutorials/published",
+    [authJwt.verifyToken],
+    tutorials.findAllPublished
+  );
+
+  // Retrieve a single Tutorial with id
+  app.get("/api/tutorials/:id",
+    [authJwt.verifyToken],
+    tutorials.findOne
+  );
+
+  // Update a Tutorial with id
+  app.put("/api/tutorials/:id",
+    [authJwt.verifyToken],
+    tutorials.update
+  );
+
+  // Delete a Tutorial with id
+  app.delete("/api/tutorials/:id",
+    [authJwt.verifyToken],
+    tutorials.delete
+  );
+
+  // Delete all Tutorials
+  app.delete("/api/tutorials/",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    tutorials.deleteAll
+  );
+
 };
