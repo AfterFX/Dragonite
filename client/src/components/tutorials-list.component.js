14687ae6499883756902459bb9e2fef5fddfe3ee
AfterFX <afterfx1995@gmail.com>
2020-11-02 00:48:46 +0200

    fixed tutorials show when no token

--- client/src/components/tutorials-list.component.js
+++ client/src/components/tutorials-list.component.js
@@ -17,7 +17,9 @@ export default class TutorialsList extends Component {
       tutorials: [],
       currentTutorial: null,
       currentIndex: -1,
-      searchTitle: ""
+      searchTitle: "",
+        retrieveTutorialsError: false,
+        retrieveTutorials: false
     };
   }
 
@@ -37,7 +39,8 @@ export default class TutorialsList extends Component {
       TutorialDataService.getAll()
           .then(response => {
               this.setState({
-                  tutorials: response.data
+                  tutorials: response.data,
+                  retrieveTutorials: true
               });
               console.log(response.data);
           })
@@ -48,7 +51,8 @@ export default class TutorialsList extends Component {
                           e.response.data &&
                           e.response.data.message) ||
                       e.message ||
-                      e.toString()
+                      e.toString(),
+                  retrieveTutorialsError: true
               });
           });
   }
@@ -77,14 +81,14 @@ export default class TutorialsList extends Component {
       })
       .catch(e => {
       this.setState({
-          auth:
+          content:
               (e.response &&
                   e.response.data &&
                   e.response.data.message) ||
               e.message ||
               e.toString()
       });
-        return notification.universal(this.state.auth, "warning");
+        return notification.universal(this.state.content, "warning");
       });
   }
 
@@ -102,103 +106,105 @@ export default class TutorialsList extends Component {
   }
 
   render() {
-    const { searchTitle, tutorials, currentTutorial, currentIndex } = this.state;
-    const auth = this.state.content;
-
-    return (
-        auth === undefined ? (//check user can see this list?
-      <div className="list row">
-        <div className="col-md-8">
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search by title"
-              value={searchTitle}
-              onChange={this.onChangeSearchTitle}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary"
-                type="button"
-                onClick={this.searchTitle}
-              >
-                Search
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <h4>Tutorials List</h4>
-
-          <ul className="list-group">
-            {tutorials &&
-              tutorials.map((tutorial, index) => (
-                <li
-                  className={
-                    "list-group-item " +
-                    (index === currentIndex ? "active" : "")
-                  }
-                  onClick={() => this.setActiveTutorial(tutorial, index)}
-                  key={index}
-                >
-                  {tutorial.title}
-                </li>
-              ))}
-          </ul>
-
-          <button
-            className="m-3 btn btn-sm btn-danger"
-            onClick={this.removeAllTutorials}
-          >
-            Remove All
-          </button>
-        </div>
-        <div className="col-md-6">
-          {currentTutorial ? (
-            <div>
-              <h4>Tutorial</h4>
-              <div>
-                <label>
-                  <strong>Title:</strong>
-                </label>{" "}
-                {currentTutorial.title}
-              </div>
-              <div>
-                <label>
-                  <strong>Description:</strong>
-                </label>{" "}
-                {currentTutorial.description}
-              </div>
-              <div>
-                <label>
-                  <strong>Status:</strong>
-                </label>{" "}
-                {currentTutorial.published ? "Published" : "Pending"}
-              </div>
-
-              <Link
-                to={"/tutorials/" + currentTutorial.id}
-                className="badge badge-warning"
-              >
-                Edit
-              </Link>
-            </div>
-          ) : (
-            <div>
-              <br />
-              <p>Please click on a Tutorial...</p>
-            </div>
-          )}
-        </div>
-      </div>
-          ) : (
+    const { searchTitle, tutorials, currentTutorial, currentIndex, retrieveTutorials, retrieveTutorialsError } = this.state;
+      return (
+        <div>
+        {retrieveTutorialsError && (
             <div className="container">
-              <header className="jumbotron">
-                <h3>{this.state.content}</h3>
-              </header>
+                <header className="jumbotron">
+                    <h3>{this.state.content}</h3>
+                </header>
             </div>
-          )
+        )}
+        {retrieveTutorials && (
+            <div className="list row">
+                <div className="col-md-8">
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search by title"
+                            value={searchTitle}
+                            onChange={this.onChangeSearchTitle}
+                        />
+                        <div className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={this.searchTitle}
+                            >
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <h4>Tutorials List</h4>
+
+                    <ul className="list-group">
+                        {tutorials &&
+                        tutorials.map((tutorial, index) => (
+                            <li
+                                className={
+                                    "list-group-item " +
+                                    (index === currentIndex ? "active" : "")
+                                }
+                                onClick={() => this.setActiveTutorial(tutorial, index)}
+                                key={index}
+                            >
+                                {tutorial.title}
+                            </li>
+                        ))}
+                    </ul>
+
+                    <button
+                        className="m-3 btn btn-sm btn-danger"
+                        onClick={this.removeAllTutorials}
+                    >
+                        Remove All
+                    </button>
+                </div>
+                <div className="col-md-6">
+                    {currentTutorial ? (
+                        <div>
+                            <h4>Tutorial</h4>
+                            <div>
+                                <label>
+                                    <strong>Title:</strong>
+                                </label>{" "}
+                                {currentTutorial.title}
+                            </div>
+                            <div>
+                                <label>
+                                    <strong>Description:</strong>
+                                </label>{" "}
+                                {currentTutorial.description}
+                            </div>
+                            <div>
+                                <label>
+                                    <strong>Status:</strong>
+                                </label>{" "}
+                                {currentTutorial.published ? "Published" : "Pending"}
+                            </div>
+
+                            <Link
+                                to={"/tutorials/" + currentTutorial.id}
+                                className="badge badge-warning"
+                            >
+                                Edit
+                            </Link>
+                        </div>
+                    ) : (
+                        <div>
+                            <br />
+                            <p>Please click on a Tutorial...</p>
+                        </div>
+                    )}
+                </div>
+
+            </div>
+        )}
+        </div>
     );
   }
 }
